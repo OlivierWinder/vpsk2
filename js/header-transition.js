833656e7cdf55d0b37a8f0b957be65826fb0bcb8
Olivier Winder <olivierwinder@gmail.com>
2023-06-23 13:50:55 +0200

    big commit

--- js/header-transition.js
+++ js/header-transition.js
@@ -1,13 +1,19 @@
 // Get the header element
 const header = document.querySelector('header');
+const headerNav = document.querySelector('.header__nav__list');
+const headerLogo = document.querySelector('.header__logo__heading');
 
 // Function to handle the scroll event
 function handleScroll() {
   // Add a CSS class to the header when the client starts scrolling
-  if (window.pageYOffset > 0) {
+  if (window.pageYOffset > 30) {
     header.classList.add('header--active');
+    headerNav.classList.add('nav--active');
+    headerLogo.classList.add('header__logo--active');
   } else {
     header.classList.remove('header--active');
+    headerNav.classList.remove('nav--active')
+    headerLogo.classList.remove('header__logo--active');
   }
 }
 
